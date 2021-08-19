8aa558d962ec71e4d9cdf886c1550ce11183d851
kavimukil <kavimukil.a@gmail.com>
2021-08-19 12:52:49 +0530

    Version bump

--- version.php
+++ version.php
@@ -21,7 +21,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021081600;
+$plugin->version = 2021081900;
 $plugin->release = "3.2+";
 $plugin->requires = 2018051700;
 $plugin->component = 'plagiarism_drillbit';
